ea88a92b94f8454632ac450749964db246d75152
carson2440 <981385016@qq.com>
2017-10-20 09:32:51 +0800

    release 0.9.8 1.新增条形图 2.新增图形缩放 3.长按移动显示高亮点

--- app/src/main/java/com/carson/chart/builder/MainActivity.java
+++ app/src/main/java/com/carson/chart/builder/MainActivity.java
@@ -12,10 +12,16 @@ import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.TypeReference;
 import com.carson.chart.builder.bean.ChartLine;
 import com.carson.chart.builder.utils.ChartUtil;
+import com.carson.chartbuilder.BMFBarLineChartTouchListener;
 import com.carson.chartbuilder.BMFLineChartRenderer;
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
@@ -40,6 +46,7 @@ public class MainActivity extends AppCompatActivity implements OnChartGestureLis
 
     TextView chartMessage;
     LineChart chartLine;
+    BarChart chartBar;
     private long serverTime;
 
 
@@ -47,10 +54,14 @@ public class MainActivity extends AppCompatActivity implements OnChartGestureLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.chartLine = (LineChart) findViewById(R.id.chart_line);
         this.chartMessage = (TextView) findViewById(R.id.chart_message);
+        this.chartLine = (LineChart) findViewById(R.id.chart_line);
+        this.chartBar = (BarChart) findViewById(R.id.chart_bar);
         initChartLine();
-        buildChartData();
+        locadChartLineData();
+
+        initChartBar();
+        locadChartBarData();
     }
 
     /**
@@ -66,6 +77,7 @@ public class MainActivity extends AppCompatActivity implements OnChartGestureLis
         chartLine.setNoDataText("暂无数据");
         chartLine.setOnChartGestureListener(this);
         chartLine.setOnChartValueSelectedListener(this);
+        chartLine.setOnTouchListener(new BMFBarLineChartTouchListener(chartLine, chartLine.getViewPortHandler().getMatrixTouch(), 3.f));
         //support scale flag
         chartLine.setTouchEnabled(true);
         chartLine.setDragEnabled(true);
@@ -90,8 +102,8 @@ public class MainActivity extends AppCompatActivity implements OnChartGestureLis
     /**
      * 从raw文件下读取json数据
      */
-    private void buildChartData() {
-        InputStream inputStream = getResources().openRawResource(R.raw.json);
+    private void locadChartLineData() {
+        InputStream inputStream = getResources().openRawResource(R.raw.json_line);
         byte buffer[] = null;
         try {
             buffer = new byte[inputStream.available()];
@@ -108,7 +120,7 @@ public class MainActivity extends AppCompatActivity implements OnChartGestureLis
             });
             if (map != null) {
                 List<ChartLine> list = map.get("42360");
-                filterData(list);
+                filterChartLineData(list);
             }
         }
     }
@@ -116,7 +128,7 @@ public class MainActivity extends AppCompatActivity implements OnChartGestureLis
     /**
      * 分段数据,设置被画线的相关属性
      */
-    private void filterData(List<ChartLine> list) {
+    private void filterChartLineData(List<ChartLine> list) {
         if (list == null) {
             return;
         }
@@ -151,6 +163,85 @@ public class MainActivity extends AppCompatActivity implements OnChartGestureLis
         chartLine.animateX(300);
 
     }
+/////////////////////////////////////////////////////////////////////////////
+
+    private void initChartBar() {
+//        chartBar.setViewPortOffsets(2f, ChartUtil.dp2px(this, 15), 0f, ChartUtil.dp2px(this, 15));
+        chartBar.getDescription().setEnabled(false);
+        chartBar.setNoDataText("暂无数据");
+        chartBar.setOnChartGestureListener(this);
+        chartBar.setOnChartValueSelectedListener(this);
+
+        chartBar.setTouchEnabled(true);
+        chartBar.setDragEnabled(true);
+        chartBar.setScaleEnabled(true);
+        // if disabled, scaling can be done on x- and y-axis separately
+        chartBar.setPinchZoom(false);
+        chartBar.setScaleXEnabled(true);
+
+
+        chartBar.getAxisRight().setEnabled(false);//不显示右边的数字
+//        chartBar.getXAxis().setEnabled(true);
+        chartBar.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        YAxis leftAxis = chartBar.getAxisLeft();
+//        leftAxis.setDrawLabels(false);
+        leftAxis.setDrawGridLines(false);
+        leftAxis.setDrawZeroLine(true);
+        chartBar.setFitBars(true);
+        Legend l = chartBar.getLegend();
+        // modify the legend ...
+        l.setForm(Legend.LegendForm.LINE);
+        l.setEnabled(false);
+
+    }
+
+    private void locadChartBarData() {
+        InputStream inputStream = getResources().openRawResource(R.raw.json_bar);
+        byte buffer[] = null;
+        try {
+            buffer = new byte[inputStream.available()];
+            inputStream.read(buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (buffer != null && buffer.length > 0) {
+            String result = new String(buffer);
+            JSONObject jsonObject = JSON.parseObject(result);
+            int state = jsonObject.getIntValue("status");
+            String dataList = jsonObject.getString("data_list");
+            Map<String, List<ChartLine>> map = JSON.parseObject(dataList, new TypeReference<Map<String, List<ChartLine>>>() {
+            });
+            if (map != null) {
+                List<ChartLine> list = map.get("50430");
+                filterChartBarData(list);
+            }
+        }
+    }
+
+    private void filterChartBarData(List<ChartLine> list) {
+        ArrayList<BarEntry> entries = new ArrayList<BarEntry>();
+        Float x, y;
+        for (int i = 0; i < list.size(); i++) {
+            ChartLine line = list.get(i);
+            x = Float.parseFloat(line.getT()) / (1000);
+            if (TextUtils.isEmpty(line.getV())) {
+                y = 0f;//无效数据
+            } else {
+                y = Float.parseFloat(line.getV());
+            }
+            entries.add(new BarEntry(i, y, line.getT()));
+        }
+        BarDataSet dataSet = new BarDataSet(entries, "ChartBar");
+//    dataSet.setColors(ColorTemplate.SINGLE_COLORS);
+//            dataSet.setHighLightAlpha(70);
+        BarData data = new BarData(dataSet);
+        data.setDrawValues(false);
+        data.setBarWidth(0.65f);
+        chartBar.setData(data);
+        chartBar.animateY(300);
+    }
+
 
     @Override
     public void onChartGestureStart(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
@@ -160,11 +251,17 @@ public class MainActivity extends AppCompatActivity implements OnChartGestureLis
     @Override
     public void onChartGestureEnd(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
         chartMessage.setText(getString(R.string.app_name));
+        chartLine.setDragEnabled(true);
     }
 
     @Override
-    public void onChartLongPressed(MotionEvent me) {
-
+    public void onChartLongPressed(MotionEvent e) {
+        chartLine.setDragEnabled(false);
+        Highlight h = chartLine.getHighlightByTouchPoint(e.getX(), e.getY());
+        if (h != null) {
+            chartLine.highlightValue(h, true);
+        }
+        chartMessage.setText("LongPressed. HI");
     }
 
     @Override
